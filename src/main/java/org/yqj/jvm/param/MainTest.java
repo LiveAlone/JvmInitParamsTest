71c04dece861d7ec69ace768102a1a6927c23494
qijunyao <qijunyao@xiaohongshu.com>
2020-08-08 14:44:21 +0800

    init

---
+++ src/main/java/org/yqj/jvm/param/MainTest.java
@@ -0,0 +1,11 @@
+package org.yqj.jvm.param;
+
+/**
+ * Description:
+ *
+ * @author yaoqijun
+ * @date 2020/8/8
+ * Email: yaoqijunmail@foxmail.com
+ */
+public class MainTest {
+}
---
+++ src/test/java/org/yqj/jvm/param/TestContent.java
@@ -0,0 +1,13 @@
+package org.yqj.jvm.param;
+
+/**
+ * Created by yaoqijun.
+ * Date:2016-02-05
+ * Email:yaoqj@terminus.io
+ * Descirbe:
+ */
+public class TestContent {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
